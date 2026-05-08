6e640a98c9e0b85cb57db96f4edcbd0beb5aabb4
GonzalezMelany <mela.glez9@gmail.com>
2026-05-08 16:08:06 -0700

    www

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9eh$jqcbctb2k4t*y!rt!188hk#!-op(a5ly5464y_h*!qyjze'
+SECRET_KEY = 'django-insecure-t%4)uz_+as#yi)_(lusxt*d!7944e$h03z-sz2-7l=z5kg(ynk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 
     'store',
 ]
-AUTH_USER_MODEL = 'store.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTH_USER_MODEL = 'store.User'
 
 ROOT_URLCONF = 'marketplace_main.urls'
 
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
